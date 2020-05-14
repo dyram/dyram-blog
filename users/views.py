b1c4b6ec3573c71275101c95caf091cc88eada97
dyram-codingmart <dyram.meyn@codingmart.com>
2020-05-14 21:44:20 +0530

    Second Commit with more Features added

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 
@@ -21,9 +22,22 @@ def register(request):
 
 
 @login_required
+@csrf_protect
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if(request.method == 'POST'):
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid()and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(
+                request, f'Your account has been updated ')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
